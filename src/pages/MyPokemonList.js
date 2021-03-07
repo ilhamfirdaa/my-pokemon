9dc3a67b2a00b48c9c4535ca30b5c91b12e33f20
ilhamfirdaa <ilhamfirdaa@gmail.com>
2021-03-07 21:08:30 +0700

    add view if doesnt catch pokemon

--- src/pages/MyPokemonList.js
+++ src/pages/MyPokemonList.js
@@ -8,20 +8,22 @@ import Swal from 'sweetalert2';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
-// context
+// contexts
 import { MyPokemonContext } from '../context/MyPokemonContext';
 
-// style
+// styles
 import {
-  container, subContainer, listPokemon, loadingContainer, loadingAnimation, btnLoadMore,
-} from '../style/global';
+  container, subContainer, listPokedex, loadingContainer, loadingAnimation,
+  btnLoadMore, emptyPokedex,
+} from '../styles/global';
 
-const MyPokemonList = () => {
+const MyPokemonList = ({ history }) => {
   document.title = 'My Pokemon | Ilham Firdaus';
   const [Pokedex, setPokedex] = useContext(MyPokemonContext);
   const [isFirstLoad, setIsFirstLoad] = useState(true);
 
-  const handleClickRelease = (nickname) => {
+  const handleClickRelease = (e, nickname) => {
+    e.stopPropagation();
     const pokeNickname = nickname[0].toUpperCase() + nickname.slice(1);
     Swal.fire({
       title: `Do you want to release ${pokeNickname}?`,
@@ -31,13 +33,23 @@ const MyPokemonList = () => {
       if (result.isConfirmed) {
         const temp = Pokedex.filter((val) => val.nickname !== nickname);
         setPokedex(temp);
-        Swal.fire(`${nickname}was released!`, '', 'success');
+        Swal.fire(`${nickname} was released!`, '', 'success');
       }
     });
   };
 
+  const handleClickDetail = (e, nickname) => {
+    e.stopPropagation();
+    history.push({
+      pathname: `/my-pokemon/${nickname}`,
+      state: nickname,
+    });
+  };
+
   useEffect(() => {
-    setIsFirstLoad(false);
+    setTimeout(() => {
+      setIsFirstLoad(false);
+    }, 1000);
   }, []);
 
   if (isFirstLoad) {
@@ -57,31 +69,64 @@ const MyPokemonList = () => {
           margin-bottom: 64px;
         `}
       >
-        {Pokedex.map((item) => (
-          <div
-            key={item.nickname}
-            role="link"
-            css={css`${listPokemon}`}
-          >
-            <img
-              src={item.image}
-              alt={item.nickname}
-              width={80}
-              height={80}
-            />
-            <span>
-              { item.nickname }
-            </span>
-
-            <button
-              type="button"
-              css={css`${btnLoadMore}`}
-              onClick={() => handleClickRelease(item.nickname)}
+        {Pokedex.length > 0
+          ? Pokedex.map((item) => (
+            <div
+              key={item.nickname}
+              role="link"
+              css={css`${listPokedex}`}
+              onClick={(e) => handleClickDetail(e, item.nickname)}
             >
-              Release
-            </button>
-          </div>
-        ))}
+              <img
+                src={item.image}
+                alt={item.nickname}
+                width="80"
+                height="80"
+              />
+              <span style={{ fontSize: '14px' }}>
+                { item.name }
+              </span>
+              <span style={{ fontWeight: '500' }}>
+                { item.nickname }
+              </span>
+
+              <button
+                type="button"
+                css={css`${btnLoadMore}`}
+                onClick={(e) => handleClickRelease(e, item.nickname)}
+              >
+                Release
+              </button>
+            </div>
+          ))
+          : (
+            <div css={css`
+              ${emptyPokedex}
+                margin: 64px 0;
+              `}
+            >
+              <img
+                src="/pikachu.png"
+                alt="pikachu"
+                height="auto"
+                width="100"
+              />
+              <span style={{ marginTop: '8px' }}>
+                You dont have any pokemon
+              </span>
+              <a href="/">
+                <button
+                  type="button"
+                  css={css`
+                  ${btnLoadMore}
+                  margin-bottom: 64px;
+                `}
+                >
+                  Lets get one
+                </button>
+              </a>
+            </div>
+          )}
       </div>
 
       <Footer />
